1379dba9e2e18d02e68c47f1cfd5fcbf2f76ca8e
dclaz <40458315+dclaz@users.noreply.github.com>
2020-03-25 23:09:01 +1100

    vignette updates

--- R/add_noise_to_outcomes.R
+++ R/add_noise_to_outcomes.R
@@ -3,7 +3,9 @@
 #' This function can be used to flip a set proportion of match outcomes. Useful
 #' for testing models.
 #'
-#' @param outcome Original match outcome. Must be between 0 and 1 inclusive.
+#' Input outcomes of 0.5 will be randomy rounded to either 0 or 1.
+#'
+#' @param outcome Original match outcome. Must be one of 0, 0.5, or 1.
 #' @param error_prob The probability an outcome switches from a 0 to 1 or
 #' vice-versa
 #'
@@ -30,12 +32,20 @@
 #'
 #' test_df$noisy_outcome = add_noise_to_outcomes(test_df$true_outcome, error_prob = 1)
 #' table(test_df)
+#'
 add_noise_to_outcomes <- function(
     outcome,
     error_prob = 0.1
 ){
 
-    noisy_outcome <- ((runif(length(outcome), 0, 1) > (1-error_prob)) + outcome) %% 2
+    if( !all(outcome %in% c(0, 0.5, 1))){
+        stop("Outcome must be one of 0, 0.5, or 1")
+    }
+
+    outcome <- round(jitter(outcome))
+
+    noise <- (runif(length(outcome), 0, 1))
+    noisy_outcome <- (outcome + (noise <= error_prob) ) %% 2
 
     return(noisy_outcome)
 
